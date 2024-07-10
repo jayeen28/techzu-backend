e61e2cd9e8a17dcc5f9d035d8579529397f09a5b
jayeen28 <jayeen6783@gmail.com>
2024-07-10 12:56:38 +0600

    boilerplate cleanup bcause it was for mysql before

--- src/utils/getLimitOffset.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (page = 1, limit = 10) {
-  if (page < 1) page = 1;
-  return [limit, (page - 1) * limit];
-}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,4 @@
-const localDateTimeParts = require('./localDateTimeParts');
-const getLimitOffset = require('./getLimitOffset');
-
 module.exports = function () {
   return {
-    localDateTimeParts,
-    getLimitOffset
   }
 }
