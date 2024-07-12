8e94cc986ef1406aeadab49d576a507dd63effc8
jayeen28 <jayeen6783@gmail.com>
2024-07-12 21:33:10 +0600

    avatar is not required

--- src/services/user/user.validate.js
+++ src/services/user/user.validate.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 module.exports.validateUserRegister = {
     body: joi.object().keys({
         full_name: joi.string().required(),
-        avatar_file_id: joi.string().required(),
+        avatar_file_id: joi.string(),
         email: joi.string().email().required(),
         password: joi.string().required().min(5).max(50)
     }),
